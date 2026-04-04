0a426be0e9240516520829a9927a4d43b329a43c
Kaleab Alemu <89988878+kaleabtesfaye01@users.noreply.github.com>
2026-04-03 20:07:10 -0400

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        '''
+        - create hashmap with sorted_str -> list of original strings structure
+        - output all values as an array
+        '''
+
+        anagram_map = defaultdict(list)
+
+        for s in strs:
+            sorted_s = ''.join(sorted(s))
+            anagram_map[sorted_s].append(s)
+
+        return [anagram_group for anagram_group in anagram_map.values()]
+
+        '''
+        For n = len(strs), and k = max length of a string in strs
+        TC: O(n * klogk)
+        SC: O(n)
+        '''
